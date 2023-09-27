82434f39fc724ec6ba17f22a6b55ab392390dc30
Leila-Rajabpour <leila.rajabpour@uon.edu.au>
2023-09-27 11:17:16 +1000

    update

--- script/NetworkJ_EGS.jl
+++ script/NetworkJ_EGS.jl
@@ -1,3 +1,4 @@
+## Running  original network j power flow without considering delay time off for the inverters
 using Pkg
 Pkg.activate("./")
 using OpenDSSDirect
@@ -11,18 +12,14 @@ using StatsPlots
 using DataFrames
 
 data_path="C:/Users/c3264323/Documents/GitHub/Network_J_Generation_Shedding/data/NVLFT_J"
-#data_path="C:/Users/bar865/Documents/GitHub/Network_J_Generation_Shedding/data/NVLFT_J"
-
 cd(data_path) # when there is an error here, check the names and address in data_path
 dss_string = open(f->read(f, String), "Master.dss")
 #OpenDSSDirect.dss(dss_string)
 const _ODSS = OpenDSSDirect
 dss(dss_string)
-
 function get_bus_voltage_snap()
     bus_dict = Dict() # Creating an Empty dictionary with name bus_dict
     phase_mapping = Dict(1=>"a", 2=>"b", 3=>"c", 4=>"n");#key,value Dictionary_name = Dict{Key_datatype, Value_datatype}("Key1" => value1, ...)
-
     for bus_name in OpenDSSDirect.Circuit.AllBusNames()
         bus_dict[bus_name]  = Dict() #Dictionary_name[key_name] # define another dict in bus_dict
         OpenDSSDirect.Circuit.SetActiveBus(bus_name)
@@ -45,11 +42,9 @@ function get_bus_voltage_snap()
     return bus_dict
 end
 bus_dict = get_bus_voltage_snap()# give the voltages of all phases of all buses
-
 function get_branch_flows_snap()
     branch_dict = Dict()
     phase_mapping = Dict(1=>"a", 2=>"b", 3=>"c", 4=>"n");
-
     for branch_name in OpenDSSDirect.PDElements.AllNames()
         branch_dict[branch_name]  = Dict()
         OpenDSSDirect.PDElements.Name(branch_name)
@@ -58,7 +53,6 @@ function get_branch_flows_snap()
         nwires = Int(length(OpenDSSDirect.CktElement.NodeOrder())/2)
         branch_flows = OpenDSSDirect.CktElement.Powers()
         branch_losses = OpenDSSDirect.CktElement.PhaseLosses()
-
         branch_dict[branch_name]["pf"] = real.(branch_flows[1:nwires])
         branch_dict[branch_name]["qf"] = imag.(branch_flows[1:nwires])
         branch_dict[branch_name]["pt"] = real.(branch_flows[nwires+1:end])
@@ -70,20 +64,19 @@ function get_branch_flows_snap()
     return branch_dict
 end
 branch_dict = get_branch_flows_snap()# give values of powers for lines
-
 solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "max_iter"=>100)
 # https://jump.dev/JuMP.jl/stable/tutorials/getting_started/getting_started_with_JuMP/#Getting-started-with-JuMP
-
 all_loads = _ODSS.Loads.AllNames() #what are our loads
 loads = Dict(ld => Any[] for ld in all_loads) #generating a dic that contain loads
-
 ## Function: Get Bus Components: Lines, Transformers, Loads
 function get_bus_components(;comp_types=["line", "transformer", "load", "gen"])::Dict{String,Any}
     bus_components = Dict{String,Any}()
     for bus_name in _ODSS.Circuit.AllBusNames()
         _ODSS.Circuit.SetActiveBus(bus_name)
+
         pde_elements = _ODSS.Bus.AllPDEatBus()
         loads = _ODSS.Bus.LoadList()
+
         bus_components[bus_name] = Dict(comp_type => Any[] for comp_type in comp_types)
         for pde_element in pde_elements
             if startswith(pde_element, "Line")
@@ -102,165 +95,187 @@ end
 # Get Bus Components 
 bus_components = get_bus_components()
 
+#determining the loads and their buses
+_ODSS.Basic.SetActiveClass("Load")    
 ##determining the loads and their buses
 _ODSS.Basic.SetActiveClass("Load")    #
 name_set=_ODSS.ActiveClass.AllNames()
 load_name_set2index=Dict(name_set[k]=>k for k=1:length(name_set))
-load_voltage = zeros(87,1)
-# df_load_voltage = DataFrame()
-# df_load_voltage[!,"Load"] = all_loads
-for TT = 1
-    load_volt =  Dict{String, Any}(ld => Any[] for ld in all_loads)
-    for ld in all_loads
-        _ODSS.Circuit.SetActiveElement(ld)
-        v_ld = _ODSS.CktElement.VoltagesMagAng()
-        # @show v_ld
-        append!(load_volt[ld], [v_ld[1,1]])
-        
-        load_voltage[load_name_set2index[ld], TT] = Array{Float64}(load_volt[ld])[1,1]
-        #df_load_voltage[df_load_Voltage."Load" ==ld,"t = $TT"] = load_volt[ld]
-    end
-    
-end
 
-bus_names = _ODSS.Circuit.AllBusNames()  #Bus names
+## computing the load voltages out of the loop
+# load_voltage=zeros(87,1000)
+# for TT=1:1000
+#     load_volt =  Dict{String, Any}(ld => Any[] for ld in all_loads)
+#     for ld in all_loads
+#         _ODSS.Circuit.SetActiveElement(ld)
+#         v_ld = _ODSS.CktElement.VoltagesMagAng()
+#         # @show v_ld
+#         append!(load_volt[ld], [v_ld[1,1]])
+        
+#         load_voltage[load_name_set2index[ld], TT] = Array{Float64}(load_volt[ld])[1,1]
+#         #df_load_voltage[df_load_Voltage."Load" ==ld,"t = $TT"] = load_volt[ld]
+#     end
+# end
+##
+bus_names = _ODSS.Circuit.AllBusNames()  #Rearranging the bus names from 1 t0 32
 number_of_buses = length(bus_names) 
-Vmag_Phase_a = zeros(number_of_buses,500)
-Vmag_Phase_b = zeros(number_of_buses,500)
-Vmag_Phase_c = zeros(number_of_buses,500)
-#first make all the positive loads negative, then raise the voltage
+
+Vmag_Phase_a = zeros(number_of_buses,1000)
+Vmag_Phase_b = zeros(number_of_buses,1000)
+Vmag_Phase_c = zeros(number_of_buses,1000)
 
 @show Vsources.AllNames()
 Vsources.Name("source")
 _ODSS.Basic.SetActiveClass("Load")
-# Loads.Name("1")
-load_profile = rand(1)*ones(1,300) #[0.7 0.75 0.68 0.7 0.76 0.89 0.8 0.92 0.95 0.9]
-all_load_profiles =Dict()
+α=0.8; # Low-pass filter parameter
+# Defining Distributions for generation and demand
+m1=8
+std1=1
+γ1 = 1+std1^2/m1^2
+μ1 = log(m1/sqrt(γ1))
+σ1 = sqrt(log(γ1))
+Pgeneration1= -rand(LogNormal(μ1,σ1),87,1001)
+Qgeneration1=0.0*ones(87,1001)
+# plot(Pgeneration1)
+# plot(Qgeneration1)
 
+m2=0.5;m3=0.2
+std2=0.2;std3=0.05;
+γ2 = 1+std2^2/m2^2
+γ3 = 1+std3^2/m3^2
+μ2 = log(m2/sqrt(γ2))
+μ3 = log(m3/sqrt(γ3))
+σ2 = sqrt(log(γ2))
+σ3 = sqrt(log(γ3))
+Pdemand1= rand(LogNormal(μ2,σ2),87,1001 )
+Qdemand1= rand(LogNormal(μ3,σ3),87,1001)
+# plot(Pdemand1)
+# plot(Qdemand1)
+
+load_profile =Pdemand1 
+all_load_profiles =Dict()
 #defining a random distribution for loads
 for ld in all_loads
-    all_load_profiles[ld] = 1.8*rand(1)
+    all_load_profiles[ld] = Pdemand1
 end
+TT = 1000
+load_kW = Pdemand1
+PV_kW = Pgeneration1 
+net_load_kW= PV_kW + load_kW # P=P_generation+P_demand
 
+load_voltage_dss = zeros(87,1000)
+load_voltage_filter = zeros(87,1000)
+load_voltage_filter[:,1]=230*ones(87,1);
 
-TT = 500
-load_kW = zeros(87,TT+1)
-PV_kW = 4*rand(87,TT+1)
-net_load_kW=zeros(87,TT+1)
+## MAIN LOOP
+# Running the simulations for 1000 sec
 
-#######
-# MAIN LOOP
-for i = 1:TT
+for i = 2:TT
  # defining the inverter at all load buses
     _ODSS.Basic.SetActiveClass("Load")
     for ld in all_loads
         _ODSS.Loads.Name(ld)
-        load_kW[parse(Int64,ld),i] = _ODSS.Loads.kW() #string to int
+        load_kW[parse(Int64,ld),i] = Pdemand1[parse(Int64,ld),i]
+        PV_kW[parse(Int64,ld),i] = Pgeneration1[parse(Int64,ld),i]
+        net_load_kW[parse(Int64,ld),i] = PV_kW[parse(Int64,ld),i]+ load_kW[parse(Int64,ld),i]
     end
     
     for ld in all_loads
-        if load_kW[parse(Int64,ld),i] >0 # for the positive loads netP =Pload-PVgen
-            _ODSS.Properties.Value("kW",string(load_kW[parse(Int64,ld),i]-PV_kW[parse(Int64,ld),i]))
-        else
-            _ODSS.Properties.Value("kW",string(load_kW[parse(Int64,ld),i]))# negative loads already considered as PV
-            
-        end
-        
-        net_load_kW[parse(Int64,ld),i] = _ODSS.Loads.kW()
-        # @show i
-        # @show load_kW[parse(Int64,ld),i] PV_kW[parse(Int64,ld),i] net_load_kW[parse(Int64,ld),i]
-        # @show PV_kW[parse(Int64,ld),i]
+        _ODSS.Properties.Value("kW",string(load_kW[parse(Int64,ld),i]+PV_kW[parse(Int64,ld),i]))   
     end
 
-      # @show net_load_kW 
-      #@show i
-      #@show load_kW
-      
-    #Rising slack bus voltage
+    #Rising the slack bus voltage
     _ODSS.Basic.SetActiveClass("Load")
     _ODSS.Loads.AllNames()
-    if  i>=50
-       _ODSS.Vsources.PU(1.15)
+    if  i>=200
+       _ODSS.Vsources.PU(0.92*1.1) # 0.06,250. 0.07 255
      else
-        _ODSS.Vsources.PU(1.0)
+        _ODSS.Vsources.PU(0.92)
     end
-    Solution.Solve()
+    #Solution.Solve()
+    _ODSS.dss("solve") # opendss solves the power flow equations
+
+    # Computing the load voltages 
+    load_volt =  Dict{String, Any}(ld => Any[] for ld in all_loads)
+    for ld in all_loads
+        _ODSS.Circuit.SetActiveElement(ld)
+        v_ld = _ODSS.CktElement.VoltagesMagAng()
+        append!(load_volt[ld], [v_ld[1,1]])
+        load_voltage_dss[load_name_set2index[ld], i] = Array{Float64}(load_volt[ld])[1,1]
+    end
+
+    #Passing the voltages through low-pass filter
+    for ld in all_loads
+        load_voltage_filter[load_name_set2index[ld], i] = α* load_voltage_filter[load_name_set2index[ld], i-1]+ (1-α)*load_voltage_dss[load_name_set2index[ld], i]
+    end
 
     #Running power flow
     bus_dict = get_bus_voltage_snap()
     branch_dict = get_branch_flows_snap()
     V_source = Vsources.PU()
-    @show V_source
-    
-    # Loads.Name("1")
-    # @show CktElement.Name(), real(CktElement.TotalPowers())
-
+     
     Vmag_Phase_a[:,i] = _ODSS.Circuit.AllNodeVmagPUByPhase(1)
     Vmag_Phase_b[:,i] = _ODSS.Circuit.AllNodeVmagPUByPhase(2)
     Vmag_Phase_c[:,i] = _ODSS.Circuit.AllNodeVmagPUByPhase(3)
 
+# Disconnection conditions of inverters 1) if V_mag_average for 5 Minutes > 258v --> disconnect inverter and keep it disconnected for 50 seconds
+                                    #   2) if V_mag >265 --> disconnect inverter and keep it disconnected for 50 seconds
+                                    # ONLY reconnect inverter if 216v < Vmag <253v for 60 seconds
+    #Condition 1                                
+    Vmag_ave= zeros(87,1);
     for ld in all_loads
-       
-            # for (bus, components) in bus_components
-            #     if ld ∈ components["load"]
-            #         append!(loads[ld], [bus])
-            #     end
-            # end
-       
-
         _ODSS.Loads.Name(ld)
-        if i> 120
-            Vmag_a_ave= sum(Vmag_Phase_a[i-120:i])/120
-            Vmag_a_ave>1.122
+        if i> 301
+            Vmag_ave[parse(Int64,ld)]= sum(load_voltage_filter[load_name_set2index[ld],i-300:i])/300
+            if Vmag_ave[parse(Int64,ld)] .> 258 #258V
                 print("true")
-            # if all(Vmag_a_ave .> 1.122) #258v
-                PV_kW[parse(Int64,ld),i+1]=0
-                #_ODSS.Properties.Value("kW",string(load_kW[parse(Int64,ld),i+1]-PV_kW[parse(Int64,ld),i+1]))
-
-            
-            if load_kW[parse(Int64,ld),i] <0
-                load_kW[parse(Int64,ld),i+1] = 0
+                if i<949
+                PV_kW[parse(Int64,ld),i+1:i+50] .= 0
+                end
+            elseif all(load_voltage_filter[load_name_set2index[ld],i-60:i] .> 216 )  &&  all(load_voltage_filter[load_name_set2index[ld],i-60:i] .< 253 )
+                   PV_kW[parse(Int64,ld),i+1] = Pgeneration1[parse(Int64,ld),i+1] 
             end
         end
 
-        # if i> 60
-        #     if Vmag_Phase_a[i-60:i] in 0.939:0.00001:1.1  #[216, 253]V
-        #         PV_kW[parse(Int64,ld),i] = PV_kW[parse(Int64,ld),i]
-        #     end
-        # end
-
-        # if i>2
-        #     Vmag_Phase_a[:,i-2] .> 1.152  #265v
-        #         PV_kW[parse(Int64,ld),i]=0
-            
-
-        # end
-
-        # if Vmag_Phase_a[:,i] .> 1.196 && i>1    #275v
-        #     load_kW[parse(Int64,ld),i][i-0.8]=0
-        # end
+        #Condition 2     
+        if i> 61 
+            if load_voltage_filter[load_name_set2index[ld],i] .> 258 # it should be 265
+                 print("true")
+                 if i<949
+                 PV_kW[parse(Int64,ld),i+1:i+50].= 0
+                 end
+            elseif all(load_voltage_filter[load_name_set2index[ld],i-60:i] .> 216) &&  all(load_voltage_filter[load_name_set2index[ld],i-60:i] .< 253)
+                   PV_kW[parse(Int64,ld),i+1] = Pgeneration1[parse(Int64,ld),i+1]
+            end
+        end
+        
     end
 end
 
-plot(Vmag_Phase_a[:,:]')
-plot(Vmag_Phase_b[:,:]')
-plot(Vmag_Phase_c[:,:]')
+plot(250*Vmag_Phase_a[:,2:1000]') #
+plot(250*Vmag_Phase_b[:,2:1000]')
+plot(250*Vmag_Phase_c[:,2:1000]')
 
-plot(load_kW[:,:]')
-plot(net_load_kW[:,:]')
-plot(PV_kW[:,:]')
+plot(load_kW[:,2:1000]')
+
+plot(net_load_kW[:,2:1000]')
+plot(load_voltage_filter[:,2:1000]') # Reverse buses 5 8 16 18 19 26 28 33 36 42 50 62 65 67 71 72 73 76 77 78 79 80 83
+plot(load_voltage_dss[10:40,2:1000]')
+
+plot(PV_kW[:,2:1000]')
+
+##
 for i in keys(bus_dict) 
     println(i)
-
   
 end
-
 print(OpenDSSDirect.Vsources.PU())
 _ODSS.CktElement.Powers()
 _ODSS.PDElements.AllPowers()
 ww=ones(1,3)
 mat=rand(1)*ones(1,3)
-#
+##
+
 
 vv = [245 248 248 249 250 257 258 259 260 258 259 255 261 259 263 258 259 256 257 259]
 pp = 10*ones(20,1)
@@ -279,7 +294,6 @@ function myLogNormal(m,std)
     γ = 1+std^2/m^2
     μ = log(m/sqrt(γ))
     σ = sqrt(log(γ))
-
     return LogNormal(μ,σ)
 end
 plot(myLogNormal(m,std))
--- script/power_flow_EGS.jl
+++ script/power_flow_EGS.jl
@@ -1,3 +1,4 @@
+## Running power flow for inverters with delay time
 using Pkg
 Pkg.activate("./")
 using OpenDSSDirect
@@ -8,18 +9,17 @@ using JuMP
 using Plots
 using Distributions
 using StatsPlots
-
+using DataFrames
+##
 data_path="C:/Users/c3264323/Documents/GitHub/Network_J_Generation_Shedding/data/NVLFT_J"
 cd(data_path) # when there is an error here, check the names and address in data_path
 dss_string = open(f->read(f, String), "Master.dss")
 #OpenDSSDirect.dss(dss_string)
 const _ODSS = OpenDSSDirect
 dss(dss_string)
-
 function get_bus_voltage_snap()
     bus_dict = Dict() # Creating an Empty dictionary with name bus_dict
     phase_mapping = Dict(1=>"a", 2=>"b", 3=>"c", 4=>"n");#key,value Dictionary_name = Dict{Key_datatype, Value_datatype}("Key1" => value1, ...)
-
     for bus_name in OpenDSSDirect.Circuit.AllBusNames()
         bus_dict[bus_name]  = Dict() #Dictionary_name[key_name] # define another dict in bus_dict
         OpenDSSDirect.Circuit.SetActiveBus(bus_name)
@@ -42,11 +42,9 @@ function get_bus_voltage_snap()
     return bus_dict
 end
 bus_dict = get_bus_voltage_snap()# give the voltages of all phases of all buses
-
 function get_branch_flows_snap()
     branch_dict = Dict()
     phase_mapping = Dict(1=>"a", 2=>"b", 3=>"c", 4=>"n");
-
     for branch_name in OpenDSSDirect.PDElements.AllNames()
         branch_dict[branch_name]  = Dict()
         OpenDSSDirect.PDElements.Name(branch_name)
@@ -55,7 +53,6 @@ function get_branch_flows_snap()
         nwires = Int(length(OpenDSSDirect.CktElement.NodeOrder())/2)
         branch_flows = OpenDSSDirect.CktElement.Powers()
         branch_losses = OpenDSSDirect.CktElement.PhaseLosses()
-
         branch_dict[branch_name]["pf"] = real.(branch_flows[1:nwires])
         branch_dict[branch_name]["qf"] = imag.(branch_flows[1:nwires])
         branch_dict[branch_name]["pt"] = real.(branch_flows[nwires+1:end])
@@ -70,45 +67,14 @@ branch_dict = get_branch_flows_snap()# give values of powers for lines
 
 solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "max_iter"=>100)
 # https://jump.dev/JuMP.jl/stable/tutorials/getting_started/getting_started_with_JuMP/#Getting-started-with-JuMP
-# for bus_name in _ODSS.Circuit.AllBusNames()
-#     _ODSS.Circuit.SetActiveBus(bus_name)
-    
-#     loads = _ODSS.Bus.LoadList()
-#     for pf in loads
-#         if pf<0
-#             pf=-pf
-#         else
-#             pf=pf
-#         end
-#     end
-# end
-
-# loads_value = branch_dict[Lines][pf]
-# print(branch_dict"[Lines(Any))]")
-# print( _ODSS.PDElements.AllPowers())
-
-m=4.5
-std=0.5
-function myLogNormal(m,std)
-    γ = 1+std^2/m^2
-    μ = log(m/sqrt(γ))
-    σ = sqrt(log(γ))
-
-    return LogNormal(μ,σ)
-end
-plot(myLogNormal(m,std))
-
-
-
 all_loads = _ODSS.Loads.AllNames() #what are our loads
 loads = Dict(ld => Any[] for ld in all_loads) #generating a dic that contain loads
-
 ## Function: Get Bus Components: Lines, Transformers, Loads
 function get_bus_components(;comp_types=["line", "transformer", "load", "gen"])::Dict{String,Any}
     bus_components = Dict{String,Any}()
     for bus_name in _ODSS.Circuit.AllBusNames()
         _ODSS.Circuit.SetActiveBus(bus_name)
-        
+
         pde_elements = _ODSS.Bus.AllPDEatBus()
         loads = _ODSS.Bus.LoadList()
 
@@ -131,144 +97,206 @@ end
 bus_components = get_bus_components()
 
 #determining the loads and their buses
-for ld in all_loads
-    for (bus, components) in bus_components
-        if ld ∈ components["load"]
-            append!(loads[ld], [bus])
-        end
-    end
-end
-# print(_ODSS.Loads.AllNames())
-# loads = Dict(ld => Any[] for ld in all_loads)
+_ODSS.Basic.SetActiveClass("Load")    #What does setactiveclass do?
+##determining the loads and their buses
+_ODSS.Basic.SetActiveClass("Load")    #
+name_set=_ODSS.ActiveClass.AllNames()
+load_name_set2index=Dict(name_set[k]=>k for k=1:length(name_set))
 
-bus_names = _ODSS.Circuit.AllBusNames()  #Bus names
+
+bus_names = _ODSS.Circuit.AllBusNames()  #Rearranging the bus names from 1 t0 32
 number_of_buses = length(bus_names) 
-Vmag_Phase_a = zeros(number_of_buses,300)
-Vmag_Phase_b = zeros(number_of_buses,300)
-Vmag_Phase_c = zeros(number_of_buses,300)
-#first make all the positive loads negative, then raise the voltage
 
-
+Vmag_Phase_a = zeros(number_of_buses,1000)
+Vmag_Phase_b = zeros(number_of_buses,1000)
+Vmag_Phase_c = zeros(number_of_buses,1000)
 
 @show Vsources.AllNames()
 Vsources.Name("source")
 _ODSS.Basic.SetActiveClass("Load")
-# Loads.Name("1")
-load_profile = rand(1)*ones(1,300) #[0.7 0.75 0.68 0.7 0.76 0.89 0.8 0.92 0.95 0.9]
-all_load_profiles =Dict()
 
+α=0.8;
+m1=6
+std1=1
+γ1 = 1+std1^2/m1^2
+μ1 = log(m1/sqrt(γ1))
+σ1 = sqrt(log(γ1))
+Pgeneration1= -rand(LogNormal(μ1,σ1),87,1001 )
+Qgeneration1=0.0*ones(87,1001)
+# plot(Pgeneration1)
+
+
+m2=3;m3=0.5
+std2=0.2;std3=0.05;
+γ2 = 1+std2^2/m2^2
+γ3 = 1+std3^2/m3^2
+μ2 = log(m2/sqrt(γ2))
+μ3 = log(m3/sqrt(γ3))
+σ2 = sqrt(log(γ2))
+σ3 = sqrt(log(γ3))
+Pdemand1= rand(LogNormal(μ2,σ2),87,1001 )
+Qdemand1= rand(LogNormal(μ3,σ3),87,1001)
+
+
+load_profile =Pdemand1 #rand(1)*ones(1,300) #[0.7 0.75 0.68 0.7 0.76 0.89 0.8 0.92 0.95 0.9]
+all_load_profiles =Dict()
 #defining a random distribution for loads
 for ld in all_loads
-    all_load_profiles[ld] = 1.8*rand(1)
+    all_load_profiles[ld] = Pdemand1#3*rand(1)
 end
+TT = 1000
+load_kW =Pdemand1#zeros(87,TT+1)
+PV_kW =Pgeneration1 #zeros(87,TT+1)#ones(87,1)*Pgeneration1'#10*rand(87,TT+1)
+net_load_kW= PV_kW + load_kW #zeros(87,TT+1)
 
-load_kW = zeros(87,300)
-PV_kW = rand(87,300)
+load_voltage_dss = zeros(87,1000)
+load_voltage_filter = zeros(87,1000)
+load_voltage_filter[:,1]=230*ones(87,1);
+########
 
-#
 # MAIN LOOP
-for i = 1:300
+
+
+
+for i = 2:TT
  # defining the inverter at all load buses
-    
     _ODSS.Basic.SetActiveClass("Load")
     for ld in all_loads
-        # _ODSS.ActiveClass.Name(ld)
         _ODSS.Loads.Name(ld)
-        load_kW[parse(Int64,ld),i] = _ODSS.Loads.kW()
-        # @show load_kW
-        if load_kW[parse(Int64,ld),i] >0 #if load>0 make it negative
-            _ODSS.Properties.Value("kW",string(-load_profile[i]*all_load_profiles[ld]*load_kW[parse(Int64,ld),i])) #make the load changing with time i
-            # load_kW[ld] = _ODSS.Loads.kW()
-        else
-            _ODSS.Properties.Value("kW",string(load_profile[i]*all_load_profiles[ld]*load_kW[parse(Int64,ld),i]))
-        end
-        load_kW[parse(Int64,ld),i] = _ODSS.Loads.kW()
+        load_kW[parse(Int64,ld),i] = Pdemand1[parse(Int64,ld),i]#_ODSS.Loads.kW() #string to int
+        PV_kW[parse(Int64,ld),i] = Pgeneration1[parse(Int64,ld),i]
+        net_load_kW[parse(Int64,ld),i] = PV_kW[parse(Int64,ld),i]+ load_kW[parse(Int64,ld),i]
     end
-    #@show i
-    @show load_kW
-    Solution.Solve()
+    
+    for ld in all_loads
+
+    #     # if load_kW[parse(Int64,ld),i] >0 # for the positive loads netP =Pload-PVgen
+         _ODSS.Properties.Value("kW",string(load_kW[parse(Int64,ld),i]+PV_kW[parse(Int64,ld),i]))
+    #     # else
+    #     #     _ODSS.Properties.Value("kW",string(-PV_kW[parse(Int64,ld),i])) #load_kW[parse(Int64,ld),i]))# negative loads already considered as PV
+            
+    #     # end
+        
+    #     net_load_kW[parse(Int64,ld),i] = PV_kW - load_kW
+    end
 
     #Rising slack bus voltage
     _ODSS.Basic.SetActiveClass("Load")
     _ODSS.Loads.AllNames()
-    if  i>=50
-       _ODSS.Vsources.PU(1.05)
+    if  i>=200
+       _ODSS.Vsources.PU(0.92*1.1)
      else
-        _ODSS.Vsources.PU(1.0)
+        _ODSS.Vsources.PU(0.92)
     end
+    #Solution.Solve()
+    _ODSS.dss("solve") # opendss solves the power flow equations
 
-    # V_source =_ODSS.Vsources.PU()
-    # @show V_source
     
+    load_volt =  Dict{String, Any}(ld => Any[] for ld in all_loads)
+    for ld in all_loads
+        _ODSS.Circuit.SetActiveElement(ld)
+        v_ld = _ODSS.CktElement.VoltagesMagAng()
+        append!(load_volt[ld], [v_ld[1,1]])
+        
+        load_voltage_dss[load_name_set2index[ld], i] = Array{Float64}(load_volt[ld])[1,1]## generated from dss
+        #df_load_voltage[df_load_Voltage."Load" ==ld,"t = $TT"] = load_volt[ld]
+    end
+    #passing through Low-pass Filter
+    for ld in all_loads
+        load_voltage_filter[load_name_set2index[ld], i] = α* load_voltage_filter[load_name_set2index[ld], i-1]+ (1-α)*load_voltage_dss[load_name_set2index[ld], i]
+    end
     #Running power flow
     bus_dict = get_bus_voltage_snap()
     branch_dict = get_branch_flows_snap()
     V_source = Vsources.PU()
-    @show V_source
-    # _ODSS.Basic.SetActiveClass("Load")
+    # @show V_source
     
-    Loads.Name("1")
-    @show CktElement.Name(), real(CktElement.TotalPowers())
+    # @show CktElement.Name(), real(CktElement.TotalPowers())
     Vmag_Phase_a[:,i] = _ODSS.Circuit.AllNodeVmagPUByPhase(1)
     Vmag_Phase_b[:,i] = _ODSS.Circuit.AllNodeVmagPUByPhase(2)
     Vmag_Phase_c[:,i] = _ODSS.Circuit.AllNodeVmagPUByPhase(3)
 
+    Vmag_ave= zeros(87,1);
+    delay_time=Array{Int64}(zeros(87,1));
     for ld in all_loads
         _ODSS.Loads.Name(ld)
-        if i> 120
-            Vmag_a_ave= sum(Vmag_Phase_a[i-120:i])/120
-            if all(Vmag_a_ave .> 1.122) #258v
-                load_kW[parse(Int64,ld),i]=0
+        if i> 301
+            Vmag_ave[parse(Int64,ld)]= sum(load_voltage_filter[load_name_set2index[ld],i-300:i])/300
+        end
+
+        if delay_time[parse(Int64,ld)] == 0
+            if PV_kW[parse(Int64,ld),i] != 0
+                if load_voltage_filter[load_name_set2index[ld],i] .> 255
+                    if i<901
+                        delay_time[parse(Int64,ld),1] =100;
+                    else
+                        delay_time[parse(Int64,ld),1] =0;
+                    end
+                    PV_kW[parse(Int64,ld),i+1:i+delay_time[parse(Int64,ld),1]] .= 0
+                # elseif Vmag_ave[parse(Int64,ld)] .> 258
+                #     if i<951
+                #         delay_time[parse(Int64,ld),1] =50;
+                #     else
+                #         delay_time[parse(Int64,ld),1] =0;
+                #     end
+                #     PV_kW[parse(Int64,ld),i+1:i+delay_time[parse(Int64,ld),1]].=0
+                end
+            elseif PV_kW[parse(Int64,ld),i] == 0 && i>61
+                if all(load_voltage_filter[load_name_set2index[ld],i-60:i] .> 216 )  &&  all(load_voltage_filter[load_name_set2index[ld],i-60:i] .< 253 )
+                    PV_kW[parse(Int64,ld),i+1] = Pgeneration1[parse(Int64,ld),i+1]
+                end 
             end
+        else
+            delay_time[parse(Int64,ld)]= delay_time[parse(Int64,ld)]-1
         end
-
-        if i> 60
-            if Vmag_Phase_a[i-60:i] in 0.939:0.00001:1.1  #[216, 253]V
-                load_kW[parse(Int64,ld),i] = load_kW[parse(Int64,ld),i]
-            end
-        end
-
-
-        if i>2
-            Vmag_Phase_a[:,i-2] .> 1.152  #265v
-                load_kW[parse(Int64,ld),i]=0
-            
-
-        end
-
-        # if Vmag_Phase_a[:,i] .> 1.196 && i>1    #275v
-        #     load_kW[parse(Int64,ld),i][i-0.8]=0
-        # end
     end
 
 
-
-
-
-
-   
-
-
+        # if i> 301
+        #     Vmag_ave= sum(load_voltage_filter[load_name_set2index[ld],i-300:i])/300
+        #     if Vmag_ave .> 258 #258V
+        #         print("true")
+        #         PV_kW[parse(Int64,ld),i+1]=0
+        #     elseif all(load_voltage_filter[load_name_set2index[ld],i-60:i] .> 216 )  &&  all(load_voltage_filter[load_name_set2index[ld],i-60:i] .< 253 )
+        #             PV_kW[parse(Int64,ld),i+1] = Pgeneration1[parse(Int64,ld),i+1] #_ODSS.Loads.KW()
+        #     end
+        # end
+      
+        # if i> 61
+        #     if load_voltage_filter[load_name_set2index[ld],i] .> 265
+        #          print("true")
+                
+        #         PV_kW[parse(Int64,ld),i+1]=0
+        #     elseif all(load_voltage_filter[load_name_set2index[ld],i-60:i] .> 216) &&  all(load_voltage_filter[load_name_set2index[ld],i-60:i] .< 253)
+        #            PV_kW[parse(Int64,ld),i+1] = Pgeneration1[parse(Int64,ld),i+1]
+        #     end
+        # end
+        
+    # end
 end
 
-plot(Vmag_Phase_a[:,:]')
-plot(Vmag_Phase_b[:,:]')
-plot(Vmag_Phase_c[:,:]')
+plot(Vmag_Phase_a[1:20,2:1000]')
+plot(Vmag_Phase_b[1:20,2:1000]')
+plot(Vmag_Phase_c[1:20,2:1000]')
+plot(load_kW[1:20,2:1000]')
 
-plot(load_kW[:,:]')
+plot(net_load_kW[1:20,2:1000]')
+plot(load_voltage_filter[1:20,2:1000]')
+plot(load_voltage_dss[1:20,2:1000]')
+plot(PV_kW[1:20,2:1000]')
 
+##
 for i in keys(bus_dict) 
     println(i)
-
   
 end
-
 print(OpenDSSDirect.Vsources.PU())
 _ODSS.CktElement.Powers()
 _ODSS.PDElements.AllPowers()
 ww=ones(1,3)
 mat=rand(1)*ones(1,3)
-#
+##
+
 
 vv = [245 248 248 249 250 257 258 259 260 258 259 255 261 259 263 258 259 256 257 259]
 pp = 10*ones(20,1)
@@ -280,4 +308,13 @@ for i = 10:20
         end
     # end
 end
-pp
+
+m=4.5
+std=0.5
+function myLogNormal(m,std)
+    γ = 1+std^2/m^2
+    μ = log(m/sqrt(γ))
+    σ = sqrt(log(γ))
+    return LogNormal(μ,σ)
+end
+plot(myLogNormal(m,std))
